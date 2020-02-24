9958437d718aba5e98f4464f83597dd71e61a00d
soufiane yakoubi <splentercell.1997@gmail.com>
2020-02-24 22:07:19 +0100

    create a training python script example

---
+++ train.py
@@ -0,0 +1,13 @@
+from network.network import Network
+from mnist.loaders import Loader, MiniBatchLoader
+
+learning_rate = 3.0
+mini_batch_size = 10
+epochs = 30
+
+training = MiniBatchLoader(Loader("data/training.gz"), mini_batch_size)
+testing = Loader("data/testing.gz")
+
+net = Network([784, 30, 10])
+
+net.SGD(training, epochs, learning_rate, testing)
