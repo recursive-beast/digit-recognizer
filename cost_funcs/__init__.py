caae6c37a38f6fabd290d69fb17b1082e4d2283c
soufiane yakoubi <splentercell.1997@gmail.com>
2020-03-03 17:10:37 +0100

    add type hints in cost_funcs

--- cost_funcs/__init__.py
+++ cost_funcs/__init__.py
@@ -14,11 +14,11 @@ from cost_funcs.types import CostFunction
 class QuadraticCost(CostFunction):
     """A class that represents the quadratic cost function"""
 
-    def __call__(self, a, y):
+    def __call__(self, a: np.ndarray, y: np.ndarray) -> float:
         """Return the cost associated with an output ``a`` and desired output ``y``"""
         return 0.5 * np.linalg.norm(a - y) ** 2
 
-    def delta(self, a, y, z):
+    def delta(self, a: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
         """Return the error delta from the output layer"""
         return (a - y) * sigmoid_prime(z)
 
@@ -26,13 +26,13 @@ class QuadraticCost(CostFunction):
 class CrossEntropyCost(CostFunction):
     """A class that represents the quadratic cost function"""
 
-    def __call__(self, a, y):
+    def __call__(self, a: np.ndarray, y: np.ndarray) -> float:
         """Return the cost associated with an output ``a`` and desired output ``y``"""
         # Note that np.nan_to_num is used to ensure numerical stability.
         # In particular, if both ``a`` and ``y`` have a 1.0 in the same slot,
         # then the expression (1-y)*np.log(1-a) returns nan.
         return np.sum(np.nan_to_num(-y * np.log(a) - (1 - y) * np.log(1 - a)))
 
-    def delta(self, a, y, z):
+    def delta(self, a: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
         """Return the error delta from the output layer"""
         return a - y
--- cost_funcs/types.py
+++ cost_funcs/types.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 class CostFunction:
     """A class that represents a cost function"""
 
-    def __call__(self, a, y):
+    def __call__(self, a: np.ndarray, y: np.ndarray) -> float:
         """Return the cost associated with an output ``a`` and desired output ``y``"""
         raise NotImplementedError
 
-    def delta(self, a, y, z):
+    def delta(self, a: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
         """Return the error delta from the output layer"""
         raise NotImplementedError
