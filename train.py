21a4c9e19011bcc4b86ae3b93b338df7d8229fcd
soufiane yakoubi <splentercell.1997@gmail.com>
2020-03-04 14:42:37 +0100

    add plot visualisation for accuracy and cost

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from network import Network
 from cost_funcs import QuadraticCost
+import matplotlib.pyplot as plt
 
 epochs = 30
 mini_batch_size = 10
@@ -10,7 +11,7 @@ cost = QuadraticCost()
 
 net = Network([784, 30, 10], cost)
 
-evaluation_accuracy, evaluation_cost, training_accuracy, training_cost = net.SGD(
+ea, ec, ta, tc = net.SGD(
     "mnist/data/training.gz",
     epochs,
     mini_batch_size,
@@ -18,4 +19,32 @@ evaluation_accuracy, evaluation_cost, training_accuracy, training_cost = net.SGD
     lmbda,
     "mnist/data/testing.gz",
     True,
+    True,
+    True,
+    True,
 )
+
+x = list(range(epochs))
+
+fig, (ax1, ax2) = plt.subplots(2, 1, True)
+
+# ----------------------------
+ax1.plot(x, ea, label="evaluation accuracy")
+ax1.plot(x, ta, label="training accuracy")
+
+ax1.set_xlabel("epochs")
+ax1.set_ylabel("accuracy")
+
+ax1.legend()
+# ----------------------------
+ax2.plot(x, ec, label="evaluation cost")
+ax2.plot(x, tc, label="training cost")
+
+ax2.set_xlabel("epochs")
+ax2.set_ylabel("total cost")
+
+ax2.legend()
+# ----------------------------
+
+plt.show()
+plt.close()
